003eed3a0c3f2b25ebd605fa6b3557c2122f66b9
firman <45849550+firmantsani@users.noreply.github.com>
2021-02-15 10:59:12 +0700

    Add files via upload

--- run.py
+++ run.py
@@ -4,9 +4,9 @@ from datetime import datetime
 import time
 print ("Chat ke sini dulu @marlboro_id_bot biar notif masuk")
 #Ganti chat id (cek disini @id_chatbot)
-chatid = '1164362761'
+chatid = '1468054653'
 #ganti kue dengan cookie akun tumbal ente
-kue = '__cfduid=da45e453d0d5fff1d18869d9c0b71de6c1613015837;aldmic-key=eyJpdiI6ImhVV2U5SVQ3c2pBaUY1VnpwbDR4Nnc9PSIsInZhbHVlIjoicGlrMjdUSDhXQ3d0NFwvZ0pkQ2hQSnZvbGltWU0rY2FlNENBb1B2NSsyRWNYTWRlczhuM3AxSGhWTktVVUFldXAiLCJtYWMiOiIzYjI5YzJiMjM4ZGE3MGNhMTA4ODc2Zjk2NjY3OGJmZjk4NzJmNDU4NjdiYTA1ZWQzOTQ3ZjZmMDYzMGFiOTE1In0%3D;XSRF-TOKEN=eyJpdiI6IkluQnJka2F6Tlp2U3d2bXY2TWpQZ3c9PSIsInZhbHVlIjoiSCtPbHJyZ2VGQUtQT1Vqeld1cHJDR216UlZjVld5c01sT3lrSjlmc01JcWZIa21HYW5JNzJYdHZ2dHYyQUtLZiIsIm1hYyI6IjU2N2IyZDlmZDlhMDk0MmM0NmQ2Y2M3ZGQ4NDdlY2RiNTAxZTczMjI2ODkzM2YxYzA5YWUyNjY5NzE3YWMwNGQifQ%3D%3D;aldmic_session=eyJpdiI6ImUySUZvQ2JLWmUzQ3NWT00yOFZZVXc9PSIsInZhbHVlIjoiZDFWdEF6S3M4WXI1M3RRY3h5cVg4dVUrTlcyTk9rcjJEQUREQ3pwZ0pSOVJ1Z0VBQllPbUZSdDV3eEFQcG1vQyIsIm1hYyI6IjU1NjA3ZTIzYWM2M2JmNTY5ZTEzODI0YThjNWMxODczNzBmZTI2ZmI1MThkMTk1YzM5ZjY3YjYyMzM4MTIyYzcifQ%3D%3D;__cf_bm=355ac0facc63e7a815ee8d6aef112ee3365d45fb-1613015845-1800-AbxUrxkyACq0u+brpkHASVzr+e6g4ZyIUxVv+9v1E4gBbL/OgRJ4iWfN/tbT29R4RgKFIUx1IZe0JNnhV6BKzRbA8t430btnRhz00Ghfx8NH8escVUqdC6FgLoF6nUbM8Q==;_gat_gtag_UA_183386867_1=1;_ga=GA1.2.1013835325.1613015760;_gid=GA1.2.621136449.1613015760;_gat_UA-102334128-3=1;_hjTLDTest=1;_hjid=5d9063db-f563-4147-93d7-2008026a01c1;_hjFirstSeen=1;_hjIncludedInPageviewSample=1;_hjAbsoluteSessionInProgress=1;_hjIncludedInSessionSample=0'
+kue = '__cfduid=da45e453d0d5fff1d18869d9c0b71de6c1613015837;_ga=GA1.2.1013835325.1613015760;_hjid=5d9063db-f563-4147-93d7-2008026a01c1;_hjTLDTest=1;_hjAbsoluteSessionInProgress=0;aldmic-key=eyJpdiI6IjBPbVlLN2VaNnJwODFpZkxzU2xNR0E9PSIsInZhbHVlIjoiN3dkREJuVVY1d2Ird3djdEFtOHlBZHV3N3k2ekU3a1lKd1NQdStUS3IyVUpZd2NsaERYRUlaTnpjcjdsekpTUiIsIm1hYyI6IjlhOTgzYTJjN2QwOWQ3NWY3ZTViMTliY2QzNTNiOWZmNzljNzI0ODcxNTEzMDdmMDZhY2ViOGZiYmRjMDE2ZWYifQ%3D%3D;__cf_bm=3d814a1d7a465740244dee743fd87df8186982d8-1613361032-1800-AbHNG+m4KNiJ+d3f53rZF7477Qv52cJAh8pzSRf1GV11YxzuvpbZ4S1+rT6jBCxIX6gNc/Ox88sSo4csH/0Zc6CLOwGEKPzdl4hj+DcFvAusboHFBDlbYAst49sAZVSDNw==;_gid=GA1.2.226113092.1613360912;_hjIncludedInSessionSample=0;XSRF-TOKEN=eyJpdiI6IlFrQ3VDdHhEXC8wWTA1TG14TndRM1pnPT0iLCJ2YWx1ZSI6IjlzODNidFNXSnFDbHZwa0JwZUNldWhoTWJxWGhCd0Jqd0J6ZEZod0o0Um9tcThiSzF6aE1sc28rQ0RHcGRZNW8iLCJtYWMiOiI1NDlhYWUyNmIyOWZjODNlYjA3NjM3N2Y3ODIzM2Q4ZDZmY2EyNTlkNzE2ZDU0NTczOTdlZjQ0NzAwZjgxZGFkIn0%3D;aldmic_session=eyJpdiI6IldNaEUxVHZBSDdVQUwzb2MzMk1XQlE9PSIsInZhbHVlIjoiZmhESkdtZmU5R0hiNnJaNzJCRmk4azc3aVBMVzM4dUU5K1NwbFQ3aVlqOFNpZWtUaTNaTHkrRzZycFFxREJnaiIsIm1hYyI6IjM4ZjAwMjEwYWNkN2IzZDRhMmZiODA5NDBkNDM3OWNkZjljZTczN2M3MjQ0MzFiNTNmODYyZTA5NjllYmU2NjAifQ%3D%3D'
 headers = ({
 'Host': 'loyalty.aldmic.com',
 'Connection': 'keep-alive',
@@ -34,5 +34,7 @@ while(True):
       get = requests.get('https://api.telegram.org/bot1642391808:AAFEXkpHnBJQ2v0zzoePlnBIWwgzL9jw9Hg/sendMessage?chat_id='+chatid+'&text='+nama[i]+"%0A"+msg)
       print (msg)
     elif redeem == 'Out of Stock':
+      #waktu = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+      msg = (redeem)
       print (redeem)
-  time.sleep(60*3)
+  time.sleep(60*1)
